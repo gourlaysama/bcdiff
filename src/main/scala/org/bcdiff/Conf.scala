b73ee297d571432c3e85ffcb2ac7597aec9ecbeb
Antoine Gourlay <antoine@gourlay.fr>
2013-04-11 18:06:51 +0200

    clean up command line arguments.

--- src/main/scala/org/bcdiff/Conf.scala
+++ src/main/scala/org/bcdiff/Conf.scala
@@ -25,13 +25,11 @@ class Conf(arg: Seq[String]) extends ScallopConf(arg) {
   // options
   val stat = opt[Boolean](descr = "Generate a diffstat.", noshort = true)
   val shortstat = opt[Boolean](descr = "output only the last line of --stat containing the number of added/modified/deleted entries.", noshort = true)
-  val contents = toggle("contents", default = Some(true), descrYes = "Diff the bytecode content of methods.",
-    descrNo = "Do not diff the bytecode, only the absence/presence of methods between the two files.", noshort = true)
-  val constantpool = toggle("constantpool", default = Some(true), descrYes = "Diff the contents of the constant pool.",
-    descrNo = "Do not diff the contents of the content pool.", noshort = true)
-  val color = toggle("color", default = Some(false), descrYes = "Show colored diff.", descrNo = "Turn off colored diff.")
+  val constantpool = opt[Boolean](default = Some(true), descr = "Diff the contents of the constant pool.",
+    noshort = true)
+  val color = opt[Boolean]("color", default = Some(false), descr = "Show colored diff.")
 
-  val files = trailArg[List[String]](descr = "Class files to diff", required = true)
+  val files = trailArg[List[String]](descr = "Class files to diff (exactly 2)", required = true)
 
   validate(files) {
     f => f.size match {
