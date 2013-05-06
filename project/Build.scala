7f32e5fbed1d623fe88398af6e9c058d6223f666
Antoine Gourlay <antoine@gourlay.fr>
2013-05-06 15:51:18 +0200

    move sbt config from build.sbt to Build.scala.

---
+++ project/Build.scala
@@ -0,0 +1,33 @@
+import sbt._
+import Keys._
+import com.typesafe.sbt.SbtStartScript
+import xerial.sbt.Pack._
+import sbtbuildinfo.Plugin._
+
+
+object BcDiffBuild extends Build {
+
+  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
+    organization := "org.bcdiff",
+    version := "0.3-SNAPSHOT",
+    scalaVersion := "2.10.1",
+    crossPaths := false
+  )
+
+  lazy val bcdiff = Project(
+    "bcdiff",
+    file("."),
+    settings = buildSettings ++ buildInfoSettings ++ packSettings ++ SbtStartScript.startScriptForClassesSettings ++
+      Seq(
+        libraryDependencies ++= Seq(
+          "org.ow2.asm" % "asm-tree" % "4.1",
+          "org.rogach" %% "scallop" % "0.8.1"
+        ),
+        scalacOptions ++= Seq("-feature", "-deprecation", "-Xlint"),
+        sourceGenerators in Compile <+= buildInfo,
+        buildInfoKeys := Seq[BuildInfoKey](name, version),
+        buildInfoPackage <<= organization,
+        packMain <<= name(n => Map(n -> "org.bcdiff.Main"))
+      )
+  )
+}
